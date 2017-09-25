28b0a8c6c28d0f72314369819fd747f07313ed25
pablito-pablito\pablito <pablo.ciento@gmail.com>
2017-09-25 21:21:30 +0200

    Init

---
+++ app.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var fs = require('fs');
+var cors = require('cors');
+
+var app = express();
+var port = 3000;
+
+app.use(cors());
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+    res.send('Server works fine, dude!');
+})
+
+app.get('/getCup', function(req, res) {
+
+    res.header("Access-Control-Allow-Origin", "*");
+    res.setHeader('Content-Type', 'application/json');
+
+    var obj = (fs.readFileSync('public/data/worldcup.json'));
+    res.send(obj);
+    // var obj = JSON.parse(fs.readFileSync('public/data/worldcup.json'));
+    // res.send(JSON.stringify(obj));
+})
+
+app.get('/txt', function(req, res) {
+
+    res.header("Access-Control-Allow-Origin", "*");
+    // res.setHeader('Content-Type', 'application/json');
+
+    var obj = fs.readFileSync('public/data/txt.txt');
+    res.send(obj);
+})
+
+app.listen(port, function(err) {
+    console.log('running server on port ' + port);
+    console.log('CORS-enabled web server listening on port  ' + port);
+});
